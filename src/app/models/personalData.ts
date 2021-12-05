4e5fb067bc973f90a50b09bed8331c7e3f9cee75
edikshsh <edikshsh@gmail.com>
2021-12-05 15:53:43 +0200

    created personal data model

---
+++ src/app/models/personalData.ts
@@ -0,0 +1,17 @@
+export class personalData {
+
+    extra = {}
+    constructor(public firstname: string,
+        public lastname: string,
+        public birthdate: Date,
+        public personalDate: Date,
+        {...kwargs}) {
+
+            if (kwargs){
+                this.extra=Object.entries(kwargs)
+            }
+         }
+
+
+
+}
